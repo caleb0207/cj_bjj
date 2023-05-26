91ed3c366668b538c5bc4bf93f0c53ef6152d608
Kyunghwan <kyunghwan0207@gmail.com>
2023-05-26 16:17:17 +0900

    Changed the HTML for the site2

--- open_mat/views.py
+++ open_mat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.utils import timezone
+from .models import Notice
 
 def post_list(request):
-    return render(request, 'open_mat/notice_list.html', {})
+    notices = Notice.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'open_mat/notice_list.html', {'notices':notices})
