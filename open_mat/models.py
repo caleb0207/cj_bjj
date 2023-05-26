f5dc7307e03af556d82af9e49e77d9d80d5ddf98
Kyunghwan <kyunghwan0207@gmail.com>
2023-05-26 19:16:29 +0900

    2023-05-26

--- open_mat/models.py
+++ open_mat/models.py
@@ -6,6 +6,7 @@ class Notice(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    location = models.CharField(max_length=200)
     event_date = models.DateTimeField(blank=True, null=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
