96ec5684e78c6377f93da3cd71dce19442606234
Kyunghwan <kyunghwan0207@gmail.com>
2023-06-01 13:50:13 +0900

    Added views to create/edit open_mat notice inside the cj_bjj.

--- open_mat/urls.py
+++ open_mat/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.notice_list, name='notice_list'),
     path('notice/<int:pk>/', views.notice_detail, name='notice_detail'),
+    path('notice/new/', views.notice_new, name='notice_new'),
+    path('notice/<int:pk>/edit/', views.notice_edit, name='notice_edit'),
 ]
--- open_mat/views.py
+++ open_mat/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Notice
 from django.shortcuts import render, get_object_or_404
+from .forms import NoticeForm
 
 def notice_list(request):
     notices = Notice.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -11,3 +13,30 @@ def notice_detail(request, pk):
     Notice.objects.get(pk=pk)
     notice = get_object_or_404(Notice, pk=pk)
     return render(request, 'open_mat/notice_detail.html', {'notice': notice})
+
+def notice_new(request):
+    if request.method == "POST":
+        form = NoticeForm(request.POST)
+        if form.is_valid():
+            notice = form.save(commit=False)
+            notice.author = request.user
+            notice.published_date = timezone.now()
+            notice.save()
+            return redirect('notice_detail', pk=notice.pk)
+    else:
+        form = NoticeForm()
+    return render(request, 'open_mat/notice_edit.html', {'form': form})
+
+def notice_edit(request, pk):
+    post = get_object_or_404(Notice, pk=pk)
+    if request.method == "POST":
+        form = NoticeForm(request.POST, instance=post)
+        if form.is_valid():
+            notice = form.save(commit=False)
+            notice.author = request.user
+            notice.published_date = timezone.now()
+            notice.save()
+            return redirect('notice_detail', pk=notice.pk)
+    else:
+        form = NoticeForm(instance=post)
+    return render(request, 'open_mat/notice_edit.html', {'form': form})
